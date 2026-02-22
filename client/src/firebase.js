58ed977f5d7da543f7b5b5734e41113a44b9e26c
Harsh <radiant.harsh2013@gmail.com>
2026-02-22 18:36:05 +0530

    Security: rotated Firebase API key and moved config to env variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdp9QFdWzd2Kar5-yCv8pqOw5PkzkyMfc",
-  authDomain: "midnight-meld.firebaseapp.com",
-  projectId: "midnight-meld",
-  storageBucket: "midnight-meld.firebasestorage.app",
-  messagingSenderId: "475894313416",
-  appId: "1:475894313416:web:49a2cdea96a0d64c8c50fd",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
